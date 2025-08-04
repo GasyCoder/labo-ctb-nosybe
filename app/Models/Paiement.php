febf4f83cf611cdb8a23fc0047f5ec637e12ac9c
GasyCoder <bezaraflorent@gmail.com>
2025-08-04 10:12:28 +0300

    Error fetching commit message for app/Models/Paiement.php

--- app/Models/Paiement.php
+++ app/Models/Paiement.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Paiement extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'prescription_id',
+        'montant',
+        'mode_paiement',
+        'recu_par',
+    ];
+
+    // Relations
+
+    public function prescription()
+    {
+        return $this->belongsTo(Prescription::class);
+    }
+
+    public function utilisateur()
+    {
+        // Le user qui a encaissé le paiement
+        return $this->belongsTo(User::class, 'recu_par');
+    }
 }
