e30c12926be1d6cd1dbd923b0cb6cdb258ea2f70
kananavy <kananavymiremby@gmail.com>
2025-08-04 23:56:21 +0300

    Sauvegarde temporaire avant pull

--- app/Livewire/Admin/Examens.php
+++ app/Livewire/Admin/Examens.php
@@ -3,11 +3,52 @@
 namespace App\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Examen; // Modèle Eloquent
 
 class Examens extends Component
 {
+    public $mode = 'list'; // 'list', 'create', 'edit', 'show'
+    public $examens;
+    public $examen;
+
+    public function mount()
+    {
+        $this->loadExamens();
+    }
+
+    public function loadExamens()
+    {
+        $this->examens = Examen::all();
+    }
+
     public function render()
     {
         return view('livewire.admin.examens');
+    }
+
+    // Méthodes pour changer de mode, charger un examen, etc.
+    public function show($id)
+    {
+        $this->examen = Examen::findOrFail($id);
+        $this->mode = 'show';
+    }
+
+    public function create()
+    {
+        $this->examen = null;
+        $this->mode = 'create';
+    }
+
+    public function edit($id)
+    {
+        $this->examen = Examen::findOrFail($id);
+        $this->mode = 'edit';
+    }
+
+    public function backToList()
+    {
+        $this->examen = null;
+        $this->mode = 'list';
+        $this->loadExamens();
     }
 }
